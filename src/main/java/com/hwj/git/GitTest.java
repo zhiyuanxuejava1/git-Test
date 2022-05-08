d793dad32d9928f408219edc8549924b35ab89e7
zhiyuan <zhiyuan@qq.com>
2022-05-08 19:49:41 +0800

    master-3 commit

--- src/main/java/com/hwj/git/GitTest.java
+++ src/main/java/com/hwj/git/GitTest.java
@@ -16,5 +16,7 @@ public class GitTest {
         System.out.println("hello git!!");
         System.out.println("hello git!!");
         System.out.println("hot-fix!!");
+        System.out.println("hot-fix!!");
+        System.out.println("hot-fix!!");
     }
 }
