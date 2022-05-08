523fc2c9acff86e57ea11d21cc44125f12086e10
zhiyuan <zhiyuan@qq.com>
2022-05-08 18:30:11 +0800

    second commit

--- src/main/java/com/hwj/git/GitTest.java
+++ src/main/java/com/hwj/git/GitTest.java
@@ -7,5 +7,7 @@ package com.hwj.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!!");
+        System.out.println("hello git1!!");
+        System.out.println("hello git2!!");
     }
 }
