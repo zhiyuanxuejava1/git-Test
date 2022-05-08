9e48d9a17a219ba8690d88c54b1391281249a8f2
zhiyuan <zhiyuan@qq.com>
2022-05-08 18:28:25 +0800

    first commit

---
+++ src/main/java/com/hwj/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.hwj.git;
+
+/**
+ * @author shkstart
+ * @create 2022-05-08  18:25
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!");
+    }
+}
---
+++ src/main/java/com/hwj/git/Test.java
@@ -0,0 +1,8 @@
+package com.hwj.git;
+
+/**
+ * @author shkstart
+ * @create 2022-05-08  18:26
+ */
+public class Test {
+}
