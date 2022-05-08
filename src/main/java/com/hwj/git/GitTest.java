3b1683802e2e67f77e3308063096729a65d84f6d
zhiyuan <zhiyuan@qq.com>
2022-05-08 18:42:21 +0800

    hot-fix-1 commit

--- src/main/java/com/hwj/git/GitTest.java
+++ src/main/java/com/hwj/git/GitTest.java
@@ -7,5 +7,8 @@ package com.hwj.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!!");
+        System.out.println("hello git!!");
+        System.out.println("hello git!!");
+        System.out.println("hello git!!");
     }
 }
